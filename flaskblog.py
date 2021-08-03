ffa6407a0f6f08f6c8b4e1dfe36fb78a4cf48fe1
gjclark14 <gjclark14@gmail.com>
2021-08-03 07:16:40 -0700

    tutorial 3 finished

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
-app = Flask(__name__)  # __name__ is the name of the module
+# __name__ is the name of the module
+app = Flask(__name__)
+
+# This key was obtained by running
+# import secrets
+# secrets.token_hex(16)
+app.config['SECRET_KEY'] = '36cec8a6d52cdf30ef725e83a4d7ce07'
 
 posts = [
     {
@@ -18,6 +25,7 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -29,6 +37,26 @@ def about():
     return render_template("about.html", title='About')
 
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login failed. Please check username and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
